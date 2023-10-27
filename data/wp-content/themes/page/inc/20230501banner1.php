129d2830e361c8400ac4d466093c6d12f24942e5
Keiko Yoshii <yoshii@arionet.jp>
2023-10-27 14:13:48 +0900

    バックアップファイル削除

--- data/wp-content/themes/page/inc/20230501banner1.php
+++
@@ -1,4 +0,0 @@
-					<div class="tac mt30 mb30 pl20 pr20 onlyPc"> <a href="<?php bloginfo('template_directory'); ?>/assets/pdf/leaflet202012.pdf" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/assets/images/bnr_02.png" alt="新型コロナウイルス感染症への安全対策について"/></a>
-					</div>
-					<div class="tac mt30 mb20 pl20 pr20 onlySp"> <a href="<?php bloginfo('template_directory'); ?>/assets/pdf/leaflet202012.pdf" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/assets/images/bnr_02sp.png" alt="新型コロナウイルス感染症への安全対策について"/></a>
-					</div>
