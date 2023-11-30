63534b84d5cfaff6bf4add6c0ab5ecd55354d9c1
Keiko Yoshii <yoshii@arionet.jp>
2023-11-30 12:34:21 +0900

    新チャットボット追加

--- data/wp-content/themes/page/footer.php
+++ data/wp-content/themes/page/footer.php
@@ -1,9 +1,37 @@
 <script>
-$(function() {
-    $('form .hasDatepicker').attr('autoComplete', 'none');
-});
-
+    $(function() {
+        $('form .hasDatepicker').attr('autoComplete', 'none');
+    });
 </script>
 <?php wp_footer(); ?>
-</body></html>
+<?php if (is_page('privacypolicy')) : ?>
+
+    <script src="https://bot.talkappi.com/assets/talkappi/talkappi.js?tid=h-kyocera-hp-1" defer="defer"></script>
+<?php else : ?>
+    <?php
+    $dir = explode('/', $_SERVER["REQUEST_URI"]);
+    $str = $dir[1];
+    switch ($str) {
+        case "en":
+            $lang = '-en';
+            break;
+        case "cn":
+            $lang = '-cn';
+            break;
+        case "tc":
+            $lang = '-tw';
+            break;
+        case "kr":
+            $lang = '-kr';
+            break;
+        default:
+            $lang = '';
+    }
+    ?>
+
+    <script src="https://bot.talkappi.com/assets/talkappi/talkappi.js?tid=h-kyocera-hp<?php echo $lang; ?>" defer="defer"></script>
+<?php endif; ?>
+</body>
+
+</html>
 <!--20230512-->
